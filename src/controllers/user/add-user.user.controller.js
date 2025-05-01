575d683466c76a4cf9dae2a7e435deb6ac0d4d6d
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2025-05-01 11:59:50 +0530

    added

--- src/controllers/user/add-user.user.controller.js
+++ src/controllers/user/add-user.user.controller.js
@@ -2,6 +2,7 @@ import { User } from "../../models/user.model.js";
 import { asyncHandler } from "../../utils/asyncHandler.js";
 import { ApiResponse } from "../../utils/ApiResponse.js";
 import { ApiError } from "../../utils/ApiError.js";
+import { generateAuthToken } from "../../helpers/generate-auth-token.helper.js";
 
 const addUser = asyncHandler(async (req, res) => {
   let existingUser = await User.findOne({ email: req.body.email });
@@ -10,8 +11,15 @@ const addUser = asyncHandler(async (req, res) => {
   }
   const user = await new User(req.body);
   await user.save();
+
+
+  const token = await generateAuthToken(user._id);
+  const loggedInUser = await User.findById(user._id).select(
+    "fullName email userRole",
+  );
+
   return res
     .status(200)
-    .json(new ApiResponse(200, {}, "User added successfully"));
+    .json(new ApiResponse(200, { user: loggedInUser, token }, "Account created successfully"));
 });
 export default addUser;
